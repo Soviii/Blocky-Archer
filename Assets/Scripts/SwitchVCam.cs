f42cfbf53704eaeb103522c93fc450de5bc12cc2
Sovi <ssonzeu@gmail.com>
2022-10-24 13:26:08 -0700

    finished player movement in correlation to camera

---
+++ Assets/Scripts/SwitchVCam.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class SwitchVCam : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private int priorityBoostAmount = 10;
+
+
+
+    private CinemachineVirtualCamera virtualCamera;
+    private InputAction aimAction;
+
+    private void Awake(){
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        aimAction = playerInput.actions["Aim"];
+    }
+
+    private void OnEnable(){
+        aimAction.performed += _  => StartAim(); //format for piping results into StartAim()
+        aimAction.canceled += _ => CancelAim();
+    }
+    private void OnDisable(){
+        aimAction.performed -= _  => StartAim();
+        aimAction.canceled -= _ => CancelAim();
+    }
+
+    private void StartAim(){
+        virtualCamera.Priority += priorityBoostAmount;
+        
+    }
+
+    private void CancelAim(){
+        virtualCamera.Priority -= priorityBoostAmount;
+    }
+}
