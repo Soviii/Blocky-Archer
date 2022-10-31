9fa57f7f67d7d006aa3849afe22381f866b394e5
Sovi <ssonzeu@gmail.com>
2022-10-30 17:56:05 -0700

    before bow n arrow implementation attempt

--- Assets/Scripts/New Inputs/NewInputs.cs
+++ Assets/Scripts/New Inputs/NewInputs.cs
@@ -4,59 +4,65 @@ using UnityEngine.InputSystem;
 public class NewInputs : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float drawingBowMultiplier = 3f;
     PlayerInputActions obj_PlayerInputActions;
     Vector2 input_movement;
     CharacterController controller;
 
     [SerializeField] float shootTimer;
     InputAction aimDowned;
+    InputAction currShooting;
+
     void Awake()
     {
         controller = GetComponent<CharacterController>();
         obj_PlayerInputActions = new PlayerInputActions();
 
-        obj_PlayerInputActions.Player.Movement.performed += x => input_movement = x.ReadValue<Vector2>();
-        aimDowned = obj_PlayerInputActions.Player.Aim;
+        aimDowned = obj_PlayerInputActions.Player.Aim; //!used for checking if player is ADS or not
         obj_PlayerInputActions.Player.Aim.performed += x => Aim();
-        obj_PlayerInputActions.Player.Shoot.performed += x => Shoot();
-        // obj_PlayerInputActions.Player.PowerfulShoot.performed += x => PowerfulShoot();
+        obj_PlayerInputActions.Player.Movement.performed += x => input_movement = x.ReadValue<Vector2>();
+        currShooting = obj_PlayerInputActions.Player.Shoot;
+        // obj_PlayerInputActions.Player.Shoot.performed += x => Shoot();
     } 
 
+    //!get shot.isPressed()
+    //!
     // Update is called once per frame
     void Update()
     {
-        CheckShootTimer();
         MovePlayer();
+        ShootingControls();
     }
     
+    void ShootingControls(){
+        if (aimDowned.IsPressed()){
+            if (currShooting.IsPressed()){
+                if (shootTimer >= 3){
+                    shootTimer = 3;
+                } else {
+                    shootTimer += Time.deltaTime * drawingBowMultiplier;
+                }
+            } else if (shootTimer > 0){
+                Debug.Log(shootTimer);
+                shootTimer = 0;
+            } else {
+                shootTimer = 0;
+            }
+        }
+    }
+
     void MovePlayer(){
         Vector2 input = obj_PlayerInputActions.Player.Movement.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
         controller.Move(move * Time.deltaTime * playerSpeed);
     }
 
-    void CheckShootTimer(){
-        if (shootTimer >= 0){
-            shootTimer -= Time.deltaTime * 2;
-        }
-    }
-
     void Jump(){
         Debug.Log("Jump!");
     }
 
-    void Shoot(){
-        if (aimDowned.IsPressed()){
-            Debug.Log("Shoot!");
-        }
-    }
-    //! use slow tap
-    void PowerfulShoot(){
-        Debug.Log("Powerful Shoot!");
-    }
-
     void Aim(){
-        Debug.Log("Aimed!");
+        Debug.Log("aimed!");
     }
 
     // Enable/Disable 
